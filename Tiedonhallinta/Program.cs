e13b0e478e7d1e42d63de02d23824a15372aaa61
Matty <Matty@DESKTOP-ANVKVJA>
2017-07-20 07:23:43 +0100

    write and save temperature to txt file

--- Tiedonhallinta/Program.cs
+++ Tiedonhallinta/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,18 @@ using System.Threading.Tasks;
 namespace Tiedonhallinta {
     class Program {
         static void Main(string[] args) {
+
+            int lampotila = 0;
+            Console.Write("Anna uusi lämpötila: ");
+            string userinput = Console.ReadLine();
+
+            lampotila = int.Parse(userinput);
+
+            string newFile = @"C:\Temp\Lämpötila.txt";
+            File.WriteAllText(newFile, lampotila.ToString());
+            Console.WriteLine("Uusi lämpötila tallennettu");
+
+            Console.ReadLine();
         }
     }
 }
